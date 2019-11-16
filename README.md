1cfea2616be98dab0d3acf0f685b0ec7f990b420
Tim Van Dyke <vandykti@mail.gvsu.edu>
2019-11-16 18:07:29 -0500

    finished for now, might add the icon later

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SimpleWeatherApp
-I followed a YouTube tutorial to create a very simple app to get weather. I have my API key in the app in plaintext so please don't abuse it
+## https://www.youtube.com/watch?v=D8-snVfekto
+
+I made this simple app based off the link above, I wanted to become familiar with how Python works with GUI's and get my feet wet with API calls in this language. I am very surprised how clean the Python code is
+
+Thank you to Keith Galli (https://www.youtube.com/channel/UCq6XkhO5SZ66N04IcPbqNcw) for putting this tutorial online
---
+++ src/WeatherApp.py
@@ -0,0 +1,68 @@
+"""
+https://www.youtube.com/watch?v=D8-snVfekto
+
+I made this simple app based off the link above, I wanted to become familiar
+with how Python works with GUI's and get my feet wet with API
+calls in this language. I am very surprised how clean the Python code is
+"""
+import tkinter as tk
+import tkinter.font
+import requests
+from PIL import Image
+from PIL import ImageTk
+
+HEIGHT = 500
+WIDTH = 600
+
+
+def formatResponse(weather):
+    print(weather)
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+        finalStr = 'City: %s \nConditions: %s \nTempurature (F): %s' % (name, desc, temp)
+    except:
+        finalStr = 'There was a problem D:\nPlease make sure that\n you have provided a City!'
+    return finalStr
+
+
+def getWeather(city):
+    weatherKey = 'd086d319958263c0e741472bc37d784f'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weatherKey, 'q': city, 'units': 'imperial'}
+    response = requests.get(url, params)
+    print('url: %s%s' % (url, params))
+    weather = response.json()
+    # change label to be correct info or error message
+    label['text'] = formatResponse(weather)
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+photoImg = Image.open('AnimeBackgroundWhiteHair.jpg')
+photoImg = photoImg.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
+
+background_image = ImageTk.PhotoImage(photoImg)
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Weather", font=('Courier', 12), command=lambda: getWeather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font=('Courier', 18))
+label.place(relwidth=1, relheight=1)
+
+root.mainloop()
